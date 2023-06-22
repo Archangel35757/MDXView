95dcad2287444a2dbc538023d996a2fa120d0d7f
DT <tempust85@hotmail.com>
2023-06-23 08:12:29 +1000

    Add the includes dir

---
+++ Includes/glm/test/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: R:/OpenGL Resources/glm-0.9.5.4/glm/test
+# Build directory: D:/dev/glm/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("bug")
+subdirs("core")
+subdirs("gtc")
+subdirs("gtx")
